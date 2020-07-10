8661ce487aea7516290fdc260d5622575d171a4c
Courtney Senior <courtney@flatfair.co.uk>
2020-07-10 12:50:55 +0100

    test: ensure that REMOVE_ITEM is handled correctly

--- src/reducers/cartReducer.test.js
+++ src/reducers/cartReducer.test.js
@@ -1,7 +1,15 @@
-import { addItem } from "../actions/cartActions";
+import { addItem, removeItem } from "../actions/cartActions";
 import cartReducer from "./cartReducer";
 
 describe("cartReducer", () => {
+  /** Setup test fixtures */
+  const testItem = {
+    id: 1,
+    name: "Pink Lady Apples",
+    description: "Crispy and delicious, with a sweet flavour.",
+    price: 35,
+  };
+
   it("should return the initial state when first called", () => {
     const expectedState = {
       products: [],
@@ -15,13 +23,6 @@ describe("cartReducer", () => {
   });
 
   it("should handle the ADD_ITEM action", () => {
-    const testItem = {
-      id: 1,
-      name: "Pink Lady Apples",
-      description: "Crispy and delicious, with a sweet flavour.",
-      price: 35,
-    };
-
     const expectedState = {
       products: [testItem],
       promotionalOffers: [],
@@ -41,6 +42,26 @@ describe("cartReducer", () => {
         },
         addItem(1)
       )
+    ).toEqual(expectedState);
+  });
+
+  it("should handle the REMOVE_ITEM action", () => {
+    const expectedState = {
+      products: [testItem],
+      promotionalOffers: [],
+      basket: [],
+      total: 0,
+      subtotal: 0,
+    };
+
+    expect(
+      cartReducer({
+        products: [testItem],
+        promotionalOffers: [],
+        basket: [testItem],
+        total: 0,
+        subtotal: 0,
+      }, removeItem(1))
     ).toEqual(expectedState);
   });
 });
