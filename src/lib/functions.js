862a6532595d1bea823e4bd391a8676576c22eb2
Courtney Senior <courtney@flatfair.co.uk>
2020-07-12 20:15:36 +0100

    feat: implement price calculation and total funcs

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -6,10 +6,16 @@ export const addToBasket = (itemId, products, basket) =>
 export const removeFromBasket = (itemId, basket) =>
   R.remove(R.findIndex(R.propEq("id", itemId), R.__), 1, R.__)(basket);
 
-import * as R from 'ramda';
+const convertPricePerKgToGram = R.divide(R.__, 1000);
+const getUnitWeight = R.view(R.lensProp("weight"));
+const getBasePrice = R.view(R.lensProp("price"));
 
-export const addToBasket = (itemId, products, basket) => R.append(R.find(R.propEq("id", itemId), products), R.__)(basket);
+const calculatePricePerWeight = R.converge(R.multiply, [getUnitWeight, R.compose(convertPricePerKgToGram, getBasePrice)]);
 
-export const removeFromBasket = (itemId, basket) => R.remove(R.findIndex(R.propEq("id", itemId), R.__), 1, R.__)(basket);
+export const getPriceForItem = item => R.ifElse(
+  R.has("weight", R.__),
+  calculatePricePerWeight(R.__),
+  getBasePrice(R.__)
+)(item);
 
-export const pricePerWeightInGrams = R.multiply(R.divide(R.__, 100));  
+export const totalAllItems = basket => R.reduce(R.add, 0, R.map(getPriceForItem, basket));
