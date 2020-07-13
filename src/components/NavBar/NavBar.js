6471e171f833549e80ca7153eacc727ff40c0f73
Courtney Senior <courtney@flatfair.co.uk>
2020-07-13 14:11:29 +0100

    feat(navbar): simple navbar UI with tests

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import { RiShoppingBasket2Line } from "react-icons/ri";
+
+const NavBar = ({ basketCount }) => (
+  <IconContext.Provider value={{ size: "2em" }}>
+    <nav className="navbar navbar-expand-md navbar-light bg-light justify-content-between" data-testid="navbar">
+      <a href="#" className="navbar-brand">
+        ShoppingCourt
+      </a>
+      <div className="navbar-nav">
+        <div className="nav-item d-flex align-items-center" data-testid="basket-nav-item">
+          <RiShoppingBasket2Line className="mr-2" />
+          {basketCount} items in basket
+        </div>
+      </div>
+    </nav>
+  </IconContext.Provider>
+);
+
+export default NavBar;
---
+++ src/components/NavBar/NavBar.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import NavBar from './NavBar';
+import { render } from '@testing-library/react';
+import faker from 'faker';
+
+describe('NavBar component', () => {
+  it('should render the NavBar and title without any errors', () => {
+    const { getByTestId } = render(<NavBar />);
+    const navbar = getByTestId("navbar");
+    expect(navbar).toBeInTheDocument();
+  });
+  
+  it('should display the correct number of items currently in the basket', () => {
+
+    const testItemsInBasketCount = faker.random.number(30);
+    const { getByTestId } = render(<NavBar basketCount={testItemsInBasketCount}/>);
+    const basketNavItem = getByTestId("basket-nav-item");
+    
+    console.log(testItemsInBasketCount);
+
+    expect(basketNavItem).toHaveTextContent(`${testItemsInBasketCount} items in basket`);
+
+    
+  });
+});
