e515bfb0063c79bd6a6aa12de8289010c15a75ba
Courtney Senior <courtney@flatfair.co.uk>
2020-07-14 21:53:35 +0100

    refactor: remove test components, final layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,8 @@
 import "./App.css";
-import AppleImg from "../../assets/Apples.jpg";
-import Basket from '../Basket/Basket';
-import ItemCard from "../ItemCard/ItemCard";
+import Basket from "../Basket/Basket";
 import ItemList from "../ItemList/ItemList";
 import NavBar from "../NavBar/NavBar";
 import React from "react";
-
-const item = {
-  id: 1,
-  name: "Pink Lady Apples",
-  image: AppleImg,
-  description: "Crisp and delicious, with a sweet flavour.",
-  price: 350,
-};
 
 function App() {
   return (
@@ -20,15 +10,7 @@ function App() {
       <NavBar basketCount={3} />
       <div className="row">
         <div className="col-md-8 col-sm-12 overflow-auto h-100">
-          <ItemList>
-            <ItemCard item={item} />
-            <ItemCard item={item} />
-            <ItemCard item={item} />
-            <ItemCard item={item} />
-            <ItemCard item={item} />
-            <ItemCard item={item} />
-            <ItemCard item={item} />
-          </ItemList>
+          <ItemList />
         </div>
         <div className="col-md-4 col-sm-12">
           <Basket />
